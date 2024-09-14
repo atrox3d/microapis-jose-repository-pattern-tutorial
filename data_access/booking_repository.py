4920f99d2c0bf39b51ce6497926b0591518e8cc6
atrox3d <atrox3d@gmail.com>
2024-09-14 20:37:45 +0200

    BookingReposistory

---
+++ data_access/booking_repository.py
@@ -0,0 +1,20 @@
+class BookingRepository:
+    def __init__(self, session) -> None:
+        self.session = session
+    
+    def get(self, **filters):
+        pass
+
+    def list(self, **filters):
+        pass
+
+    def add(self, **kwargs):
+        pass
+
+    def update(self, **kwargs):
+        pass
+
+    def delete(self, id_):
+        pass
+
+
